f76bff886fca9a9d5b346009be072a9cd2ee8bca
Kamila <kamila.dynysiuk@gmail.com>
2021-03-06 19:20:26 +0000

    add map

--- address-map/map.php
+++ address-map/map.php
@@ -10,8 +10,7 @@ function display_map()
     $content .= '<div id="primary" class="content-area">
     <div class="map-container">
     <div id="mapid" style="width: 100%; height: 500px; margin-top: 50px;"> </div>
-</div>
-</div>';
+</div></div>';
 
     return $content;
 }
