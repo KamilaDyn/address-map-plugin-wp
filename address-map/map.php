8306b429b030a953db5982302f75687eaf0c34ce
Kamila <kamila.dynysiuk@gmail.com>
2021-03-06 23:57:22 +0000

    debug unisntall

--- address-map/map.php
+++ address-map/map.php
@@ -14,9 +14,3 @@ function display_map()
 
     return $content;
 }
-
-global $wpdb;
-$sql_name = " SELECT * FROM wp_posts INNER JOIN wp_postmeta on wp_posts.ID = wp_postmeta.post_id WHERE wp_posts.post_type = 'address_map' ORDER BY wp_posts.ID;";
-
-
-$result = $wpdb->get_results($sql_name);
--- address-map/uninstall.php
+++ address-map/uninstall.php
@@ -14,4 +14,4 @@ global $wpdb;
 $tablePostMeta = $wpdb->prefix . 'postmeta';
 $tablePosts = $wpdb->prefix . 'posts';
 $wpdb->query("DELETE FROM $tablePosts WHERE post_type = 'address_map'");
-$wpdb->query("DELETE FROM $tablePostMeta WHERE post_id NOT IN (SELECT id FROM wp_posts WHERE post_type='address_map')");
+$wpdb->query("DELETE FROM $tablePostMeta WHERE post_id NOT IN (SELECT id FROM wp_posts");
